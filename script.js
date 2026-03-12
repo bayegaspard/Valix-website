bba3922fde57e4d87bd2fe475eee08cc35473f48
Dr. Bae <gaspybaye@gmail.com>
2026-03-12 01:28:01 -0400

    updated code to fix return and boxes for others not appearing

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
   initFadeInAnimations();
   initVideoPlayButton();
   initWaitlistDynamicFields();
+  initFormEnhancements();
 });
 
 // ===== NAVIGATION FUNCTIONALITY =====
